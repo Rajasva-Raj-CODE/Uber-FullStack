b9b722c98b65f6c5fcf73a5546f5c55fdb551751
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-11-27 22:06:16 +0530

    database connection done

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
-const app = express();
+import connectDB from './DB/db.js';
 
 dotenv.config();
+connectDB();
+const app = express();
 app.use(cors());
+
 
 app.get('/',(req,res)=>{
     res.send('Hello World!')
